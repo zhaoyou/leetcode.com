3a312d7ef97af88d6f85a9e2f33e779db65a81b9
zhaoyou <zhaoyou.xt@gmail.com>
2017-02-24 17:01:45 +0800

    add binary_search for median of two sorted arrays

--- algorithms/src/MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.java
+++ algorithms/src/MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.java
@@ -36,6 +36,49 @@ public class MedianOfTwoSortedArrays {
         }
     }
 
+    /**
+     * 二分查找采用递归的方法.
+     * @param arr
+     * @param start
+     * @param end
+     * @param kkey
+     * @return
+     */
+    public int binary_search(int[] arr, int start, int end, int kkey) {
+        if (start > end) return -1;
+
+        int mid = start + (end - start) / 2;
+        if (arr[mid] > kkey) {
+            return binary_search(arr, start, mid - 1, kkey);
+        } else if (arr[mid] < kkey) {
+            return binary_search(arr, mid + 1, end, kkey);
+        } else {
+            return mid;
+        }
+    }
+
+    /**
+     * 二分查找采用while循环
+     * @param arr
+     * @param start
+     * @param end
+     * @param kkey
+     * @return
+     */
+    public int binary_search_while(int[] arr, int start, int end, int kkey) {
+        while(start <= end) {
+            int mid = start + (end - end) / 2;
+            if (arr[mid] > kkey) {
+                end = mid - 1;
+            } else if (arr[mid] < kkey) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
     @Test
     public void test() {
         int[] num1 = new int[]{1, 2};
@@ -48,5 +91,11 @@ public class MedianOfTwoSortedArrays {
         int[] num1 = new int[]{1, 3};
         int[] nums2 = new int[]{2};
         System.out.println("The median is " + findMedianSortedArrays(num1, nums2));
+    }
+
+    @Test
+    public void test_search() {
+        int [] arr = new int[]{2,9, 11, 13, 19, 23};
+        System.out.println(binary_search_while(arr, 0, arr.length - 1, 20));
     }
 }
