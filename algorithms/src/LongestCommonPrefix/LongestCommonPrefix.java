faba7b2aec9a379c139ddd3920aa0cc2127322a6
zhaoyou <zhaoyou.xt@gmail.com>
2018-09-04 11:22:36 +0800

    [longestCommonprefix] update

--- algorithms/src/LongestCommonPrefix/LongestCommonPrefix.java
+++ algorithms/src/LongestCommonPrefix/LongestCommonPrefix.java
@@ -5,8 +5,8 @@ public class LongestCommonPrefix {
         String[] source = {"flower","flow","flight"};
         String[] source2 = {"dog","racecar","car"};
 
-        System.out.println(longestCommonPrefix2(source));
-        System.out.println(longestCommonPrefix2(source2));
+        System.out.println(longestCommonPrefix3(source));
+        System.out.println(longestCommonPrefix3(source2));
 
     }
 
@@ -32,6 +32,11 @@ public class LongestCommonPrefix {
         return result;
     }
 
+    /**
+     * 水平比较
+     * @param strs
+     * @return
+     */
     public static String longestCommonPrefix2(String[] strs) {
         if (strs.length == 0) {return "";}
         String result = strs[0];
@@ -40,6 +45,25 @@ public class LongestCommonPrefix {
                 result = result.substring(0, result.length() - 1);
                 if (result.isEmpty())
                     return "";
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 垂直比较
+     * @param strs
+     * @return
+     */
+    public static String longestCommonPrefix3(String[] strs) {
+        if (strs.length == 0) return "";
+        String result = strs[0];
+        for(int i = 0; i < result.length(); i++) {
+            char c = result.charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (c != strs[j].charAt(i) || i == strs[j].length()) {
+                    return result.substring(0, i);
+                }
             }
         }
         return result;
