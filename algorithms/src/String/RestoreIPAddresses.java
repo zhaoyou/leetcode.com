db6e1b797640941a4521ead4f1d04fecc0241d0b
zhaoyou <zhaoyou.xt@gmail.com>
2018-11-12 22:19:34 +0800

    [Restore IP Addresses] add

---
+++ algorithms/src/String/RestoreIPAddresses.java
@@ -0,0 +1,57 @@
+package String;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestoreIPAddresses {
+
+
+    @Test
+    public void test() {
+        String s = "1222212342";
+        System.out.println(restoreIpAddresses(s));
+    }
+
+    public List<String> restoreIpAddresses(String s) {
+
+        List<String> list = new ArrayList<>();
+
+        if (s == null || s.length() < 4 || s.length() > 12) return list;
+
+
+        dfs(s, 0, "", list);
+        return list;
+    }
+
+    public void dfs(String s, int start, String item, List<String> list) {
+
+        if (start == 3 && isValidNumber(s)) {
+            list.add(item + s);
+            return;
+        }
+
+        for(int i = 0; i < 3 && i < s.length() - 1; i++) {
+            String subStr = s.substring(0, i + 1);
+
+            if (isValidNumber(subStr)) {
+                dfs(s.substring(i + 1), start + 1, item + subStr + ".", list);
+            }
+        }
+
+        return;
+    }
+
+    private boolean isValidNumber(String s) {
+        if (s.charAt(0) == '0')
+            return s.equals("0");
+
+        int num = Integer.parseInt(s);
+
+        if (num < 0 || num > 255) return false;
+
+        return true;
+    }
+
+}
