bbf4b0510a26b13bcd4d35b5fb06f03ad78e7c05
zhaoyou <zhaoyou.xt@gmail.com>
2018-11-26 16:29:19 +0800

    120. Triangle [option]

--- algorithms/src/DP/Triangle.java
+++ algorithms/src/DP/Triangle.java
@@ -3,7 +3,6 @@ package DP;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -23,19 +22,49 @@ import java.util.List;
  *
  * Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
  */
+
+/**
+ * reference: https://blog.csdn.net/Jin_Kwok/article/details/51418353
+ */
 public class Triangle {
 
     @Test
     public void test() {
-        List list1 = Arrays.asList(-1);
-        List list2 = Arrays.asList(2, 3);
-        List list3 = Arrays.asList(1, -1, -3);
-//        List list4 = Arrays.asList(4, 1, 8, 3);
+        List list1 = Arrays.asList(2);
+        List list2 = Arrays.asList(3, 4);
+        List list3 = Arrays.asList(6, 5, 7);
+        List list4 = Arrays.asList(4, 1, 8, 3);
 
-        List triangle = Arrays.asList(list1, list2, list3);
+        List triangle = Arrays.asList(list1, list2, list3, list4);
 
-        System.out.println(minimumTotal(triangle));
+        System.out.println(minimumTotal2(triangle));
 
+    }
+
+    public int minimumTotal2(List<List<Integer>> triangle) {
+
+        if (triangle.size() == 1) return triangle.get(0).get(0);
+
+        int len = triangle.size();
+
+        int dp[] = new int[len]; // 使用一个数组就存放
+
+        for(int j = 0; j < triangle.get(len - 1).size(); j ++) {
+            dp[j] = triangle.get(len - 1).get(j);
+        }
+
+        for (int i = len - 2; i >=0; i--) {
+            List<Integer> subList = triangle.get(i);
+
+            for(int j = 0; j < subList.size(); j++) {
+                int num1 = dp[j] + triangle.get(i).get(j);
+                int num2 = dp[j + 1] + triangle.get(i).get(j);
+
+                dp[j] = Math.min(num1, num2); // 定义一个一维数组，每次保留一行的最优解，到最终 dp[0] 就是结果
+            }
+        }
+
+        return dp[0];
     }
 
 
