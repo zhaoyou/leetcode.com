f2cf1df54edb676dd52aea4fddade1534861bf7b
zhaoyou <zhaoyou.xt@gmail.com>
2019-01-14 22:57:43 +0800

    39. Combination Sum

---
+++ algorithms/src/DP/CombinationSum.java
@@ -0,0 +1,35 @@
+package DP;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+
+    @Test
+    public void test() {
+        int[] candidates = {2,3,5};
+        int target = 8;
+        System.out.println(combinationSum(candidates, target));
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> list = new ArrayList<>();
+        dfs(list, new ArrayList<>(), candidates, target, 0);
+        return list;
+    }
+
+    private void dfs(List<List<Integer>> list, List<Integer> tempList, int[] candidates, int remain, int start) {
+        if (remain < 0) return;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else {
+            for(int i = start; i < candidates.length; i++) {
+                tempList.add(candidates[i]);
+                dfs(list, tempList, candidates, remain - candidates[i], i);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
