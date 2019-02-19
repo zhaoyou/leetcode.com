7aa886ae0333e083ddbdc2addbe56882595a6385
zhaoyou <zhaoyou.xt@gmail.com>
2019-02-19 20:50:14 +0800

    240. Search a 2D Matrix II

---
+++ algorithms/src/binarysearch/SearchA2DMatrixII.java
@@ -0,0 +1,75 @@
+package binarysearch;
+
+import org.junit.Test;
+
+/**
+ * reference:https://leetcode.com/problems/search-a-2d-matrix-ii/discuss/66140/My-concise-O(m+n)-Java-solution
+ */
+public class SearchA2DMatrixII {
+
+    /**
+     * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+     *
+     * Integers in each row are sorted in ascending from left to right.
+     * Integers in each column are sorted in ascending from top to bottom.
+     * Example:
+     *
+     * Consider the following matrix:
+     *
+     * [
+     *   [1,   4,  7, 11, 15],
+     *   [2,   5,  8, 12, 19],
+     *   [3,   6,  9, 16, 22],
+     *   [10, 13, 14, 17, 24],
+     *   [18, 21, 23, 26, 30]
+     * ]
+     * Given target = 5, return true.
+     *
+     * Given target = 20, return false.
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int rows = matrix.length;
+        int colums = matrix[0].length;
+
+        if (target < matrix[0][0] || target > matrix[rows - 1][colums - 1]) return false;
+
+        int row = 0, col = colums - 1;
+        while(row < rows && col >=0) {
+            if (matrix[row][col] == target) return true;
+
+            if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+
+
+    @Test
+    public void test() {
+        int matrix [][] = {
+                {1,   4,  7, 11, 15},
+                {2,   5,  8, 12, 19},
+                {3,   6,  9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+
+        int target = 20;
+        System.out.println(searchMatrix(matrix, target));
+    }
+
+
+}
