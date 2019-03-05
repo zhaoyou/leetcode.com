2d87c541d9ed1d8eddcd3aa66666c056d5510b2d
zhaoyou <zhaoyou.xt@gmail.com>
2019-03-05 15:20:41 +0800

    update KthSmallestElementInASortedMatrix.java

--- algorithms/src/stack/KthSmallestElementInASortedMatrix.java
+++ algorithms/src/stack/KthSmallestElementInASortedMatrix.java
@@ -14,17 +14,17 @@ public class KthSmallestElementInASortedMatrix {
 
     @Test
     public void test() {
-//        int [][] matrix = {
-//                {1, 5, 9},
-//                {10,11,13},
-//                {12,13,15}
-//        };
         int [][] matrix = {
-                {1,2},
-                {1,3}
+                {1, 5, 9},
+                {10,11,13},
+                {12,13,15}
         };
-        int k = 2;
-        System.out.println(kthSmallest(matrix, k));
+//        int [][] matrix = {
+//                {1,2},
+//                {1,3}
+//        };
+        int k = 8;
+        System.out.println(kthSmallest2(matrix, k));
     }
 
     public int kthSmallest(int[][] matrix, int k) {
@@ -43,5 +43,52 @@ public class KthSmallestElementInASortedMatrix {
             }
         }
         return queue.peek();
+    }
+
+
+
+    public int kthSmallest2(int[][] matrix, int k) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int left = matrix[0][0], right = matrix[rows-1][cols-1];
+
+        while(left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            //System.out.println("left: " + left + " right: " + right + " mid: " + mid);
+
+            int count = findLessThanTarget(matrix, mid);
+
+
+            if (count < k) left = mid + 1;
+            else {
+                right = mid ;
+            }
+        }
+
+        return left;
+    }
+
+    /**
+     * 找到不大于某个target的个数
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public int findLessThanTarget(int[][] matrix, int target) {
+        int res = 0;
+        int rows = matrix.length, cols = matrix[0].length;
+        int i = rows - 1, j = 0;
+
+        while(i >= 0 && j < cols) {
+            if (matrix[i][j] <= target) {
+                res += i + 1;
+                j++;
+            } else {
+                i--;
+            }
+        }
+
+        return res;
     }
 }
