3a8bed3ff38858668ffb0ebac2b021e717a1ea01
zhaoyou <zhaoyou.xt@gmail.com>
2019-04-03 12:12:32 +0800

    334. Increasing Triplet Subsequence

---
+++ algorithms/src/interviewMedium/ArrayAndString/IncreasingTripletSubsequence.java
@@ -0,0 +1,13 @@
+package interviewMedium.ArrayAndString;
+
+public class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        int small = Integer.MAX_VALUE, big =Integer.MAX_VALUE;
+        for(int num: nums) {
+            if (num <= small) small = num;
+            else if (num <= big) big = num;
+            else return true;
+        }
+        return false;
+    }
+}
