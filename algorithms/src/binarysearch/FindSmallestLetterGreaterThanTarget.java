845285b65bc6c3189ff41c444a7fc671e9a58ad1
zhaoyou <zhaoyou.xt@gmail.com>
2019-11-12 21:42:58 +0800

    744. Find Smallest Letter Greater Than Target

--- algorithms/src/binarysearch/FindSmallestLetterGreaterThanTarget.java
+++ algorithms/src/binarysearch/FindSmallestLetterGreaterThanTarget.java
@@ -3,6 +3,10 @@ package binarysearch;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * reference
+ * https://leetcode.com/problems/find-smallest-letter-greater-than-target/discuss/110005/Easy-Binary-Search-in-Java-O(log(n))-time
+ */
 public class FindSmallestLetterGreaterThanTarget {
 
     public char nextGreatestLetter(char[] letters, char target) {
@@ -14,22 +18,23 @@ public class FindSmallestLetterGreaterThanTarget {
 
             int mid = start + (right - start) / 2;
 
-            if (letters[mid] <= target) {
+            if (letters[mid] <= target) {  // 直到找到第一个最大的值
                 start = mid + 1;
             } else {
                 right = mid;
             }
         }
 
-        if (right >= letters.length) {
-            if (letters[right - 1] <= target) {
-                right = 0;
-            } else {
-                right -=1;
-            }
-        }
+        // 直接考虑 start % letters.length   (终止条件是 start == right)
+//        if (right >= letters.length) {
+//            if (letters[right - 1] <= target) {
+//                right = 0;
+//            } else {
+//                right -=1;
+//            }
+//        }
 
-        return letters[right];
+        return letters[start % letters.length]; // 考虑若没有找到合适的，则left到达letters.length 则直接采用 start % letters.length
     }
 
     @Test
