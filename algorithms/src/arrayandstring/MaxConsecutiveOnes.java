b9e900da2fb7274c1fc710b50b074f431ee6ea4e
zhaoyou <zhaoyou.xt@gmail.com>
2019-12-02 22:09:23 +0800

    485. Max Consecutive Ones

--- algorithms/src/arrayandstring/MaxConsecutiveOnes.java
+++ algorithms/src/arrayandstring/MaxConsecutiveOnes.java
@@ -1,4 +1,21 @@
 package arrayandstring;
 
+import org.junit.Test;
+
 public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = -1;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) count++;
+            else { max = Math.max(max, count); count = 0;}
+        }
+        return Math.max(max, count);
+    }
+
+    @Test
+    public void test() {
+        int[] nums = {1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
 }
