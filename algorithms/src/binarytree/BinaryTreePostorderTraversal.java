966d23a8176c04fc131cebde4c4340517275f2f2
zhaoyou <zhaoyou.xt@gmail.com>
2019-12-08 22:39:51 +0800

    45. Binary Tree Postorder Traversal

--- algorithms/src/binarytree/BinaryTreePostorderTraversal.java
+++ algorithms/src/binarytree/BinaryTreePostorderTraversal.java
@@ -3,9 +3,34 @@ package binarytree;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePostorderTraversal {
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        if (root == null) return list;
+
+        stack.add(root);
+
+        while(!stack.isEmpty()) {
+
+            TreeNode node = stack.pop();
+
+            list.add(0, node.val);
+
+            if (node.left != null) {stack.push(node.left);}
+
+            if (node.right !=null) { stack.push(node.right);}
+        }
+        return list;
+    }
+
+
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         helper(root, list);
@@ -31,7 +56,8 @@ public class BinaryTreePostorderTraversal {
         root.right = rightNode;
         rightNode.left = leftNode;
 
-        System.out.println(postorderTraversal(root));
+       // System.out.println(postorderTraversal(root));
+        System.out.println(postorderTraversal2(root));
 
     }
 }
