b4fb22f29ae4a03b8900be1653885874e64dd84f
zhaoyou <zhaoyou.xt@gmail.com>
2019-12-09 21:15:38 +0800

    [binary tree] inorder traversal

--- algorithms/src/binarytree/BinaryTreeInorderTraversal.java
+++ algorithms/src/binarytree/BinaryTreeInorderTraversal.java
@@ -4,11 +4,33 @@ import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
+/**
+ * reference
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/45551/Preorder-Inorder-and-Postorder-Iteratively-Summarization
+ */
 public class BinaryTreeInorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
 
+        if (root == null) return list;
+
+        TreeNode cur = root;
+
+        while(cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+            } else {
+                cur = stack.pop();
+                list.add(cur.val);
+                cur = cur.right;
+            }
+        }
         return list;
     }
 
@@ -37,7 +59,7 @@ public class BinaryTreeInorderTraversal {
         root.right = rightNode;
         rightNode.left = leftNode;
 
-         System.out.println(inorderTraversal2(root));
+         System.out.println(inorderTraversal(root));
 
     }
 
