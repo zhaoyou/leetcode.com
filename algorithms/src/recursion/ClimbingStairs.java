d82b4a6119114c183249d4dd8fab1bee04771fc2
zhaoyou <zhaoyou.xt@gmail.com>
2020-02-27 15:25:00 +0800

    70. Climbing Stairs

---
+++ algorithms/src/recursion/ClimbingStairs.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int[] arr = new int[n + 1];
+
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for(int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
