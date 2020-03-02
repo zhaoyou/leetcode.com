26bb43fe714735d7070fc6eb78575b0692886238
zhaoyou <zhaoyou.xt@gmail.com>
2020-03-02 12:40:14 +0800

    96. Unique Binary Search Trees

---
+++ algorithms/src/recursion/UniqueBinarySearchTrees.java
@@ -0,0 +1,20 @@
+package recursion;
+
+/**
+ * https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
+ * https://leetcode.com/problems/unique-binary-search-trees/discuss/31707/Fantastic-Clean-Java-DP-Solution-with-Detail-Explaination
+ * 
+ */
+public class UniqueBinarySearchTrees {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
