03451962a207f62e0ffccc08c384ce50be40860b
zhaoyou <zhaoyou.xt@gmail.com>
2020-10-17 22:15:54 +0800

    [rotatelist]

--- algorithms/src/ListNode/RotateList.java
+++ algorithms/src/ListNode/RotateList.java
@@ -1,6 +1,7 @@
 package ListNode;
 
 
+import org.junit.Test;
 
 public class RotateList {
 
@@ -71,5 +72,68 @@ public class RotateList {
                     ", next=" + next +
                     '}';
         }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+
+        ListNode p = head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+
+        int i = 0;
+
+        while(p != null) {
+            i++;
+            p = p.next;
+        }
+
+
+
+        int step =  k % i ;
+
+        if (step == 0) return head;
+
+        p = head;
+        i = 0;
+
+        ListNode slow = dummy;
+
+
+        while(p != null) {
+            p  = p.next;
+            i++;
+            if (i > step)  slow = slow.next;
+        }
+
+
+        ListNode next = slow.next;
+        slow.next = null;
+
+        p = next;
+
+
+        while(next.next != null) {
+            next = next.next;
+        }
+
+        next.next = head;
+
+        return p;
+    }
+
+    @Test
+    public void test() {
+        RotateList.ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+//        head.next.next.next.next.next = new ListNode(6);
+//        head.next.next.next.next.next.next = new ListNode(7);
+
+
+        ListNode r = rotateRight(head, 2);
+        System.out.println(r);
     }
 }
